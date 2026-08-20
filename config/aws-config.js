a92c7b4c366fb39b5ff0c08b87b2c204dacf566f
Manasvi Jadhav <manasvisrj05@gmail.com>
2026-08-20 12:13:10 +0530

    changed env variables

--- config/aws-config.js
+++ config/aws-config.js
@@ -1,8 +1,16 @@
 const AWS = require("aws-sdk");
 
-AWS.config.update({region:"ap-south-1"});
+AWS.config.update({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
 const s3 = new AWS.S3();
-const S3_BUCKET = "versioncontrolmanasvi";
 
-module.exports={s3,S3_BUCKET};
+const S3_BUCKET = process.env.S3_BUCKET;
+
+module.exports = {
+    s3,
+    S3_BUCKET
+};
